a1e116ba527ce092655712322f81697967a6c29c
hiro-47 <13160306@g.matsuyama-u.ac.jp>
2019-11-11 18:23:55 +0900

    Initial Commit

---
+++ tryagain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tryagain
+//
+//  Created by KATAOKA  HIROTAKA on 2019/11/11.
+//  Copyright © 2019 hirotaka.kataoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
